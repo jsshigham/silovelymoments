1dea67cb8a01e50b400268bad05324ab6184da50
Jonathon Higham <109483370+jsshigham@users.noreply.github.com>
2024-01-08 21:14:45 -0600

    homePage dev tags and stayles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,17 @@ import "./homePage.css";
 function Home() {
   // const { screenWidth } = useContext(MyContext)!;
 
+  const section2Images = [
+    { title: "img1", alt: "Wedding Couple with close up on flowers" },
+    { title: "img2", alt: "Baby lying on a bed" },
+    { title: "img3", alt: "Man holding a woman in a corn field" },
+  ];
+  const section3ImagesSmall = [
+    { title: "imgSmall1", alt: "Kids playing in a park" },
+    { title: "imgSmall2", alt: "Daughter next to pregnant mother" },
+    { title: "imgSmall3", alt: "Couple lost in each others eyes at the bay" },
+  ];
+
   return (
     <main className="homePage">
       <section className="homeSection1">
@@ -15,13 +26,41 @@ function Home() {
         />
       </section>
       <section className="homePageSection2">
-        <img className="section2Img" src="/homePage/section2img1.jpeg" alt="" />
-        <img className="section2Img" src="/homePage/section2img2.jpeg" alt="" />
-        <img className="section2Img" src="/homePage/section2img3.jpeg" alt="" />
+        {section2Images.map((image) => (
+          <img
+            className="section2Img"
+            src={`/homePage/section2/${image.title}.jpeg`}
+            alt={image.alt}
+          />
+        ))}
       </section>
-      <a className="homePageSection3" href="/contact">
-        Book a Session
+      <a className="homePageBookASessionBtn" href="/contact">
+        BOOK A SESSION
       </a>
+      <section className="homePageSection3">
+        <div className="homePageSection3ImageLarge">
+          <img
+            src="/homePage/section3/imgLarge.jpeg"
+            alt="A family sitting down and enjoying the beach at sunset"
+            id="section3ImgLarge"
+          />
+        </div>
+        <div className="homePageSection3ImagesSmall">
+          {section3ImagesSmall.map((image) => (
+            <img
+              className="section3ImgSmall"
+              src={`/homePage/section3/${image.title}.jpeg`}
+              alt={image.alt}
+            />
+          ))}
+        </div>
+      </section>
+      <span className="homePageText">MOMENTS WORTH EVERYTHING</span>
+      <img
+        src="/homePage/section4/imgLarge.jpeg"
+        alt="Father, Mother, Daughter and Son enjoying the beach"
+        id="section4ImgLarge"
+      />
     </main>
   );
 }
