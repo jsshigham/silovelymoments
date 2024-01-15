9185665c7efdaed734bc0f5cac9cda89537a285a
Jonathon Higham <109483370+jsshigham@users.noreply.github.com>
2024-01-15 13:39:14 -0700

    sessions page styled

--- src/pages/Sessions/Sessions.tsx
+++ src/pages/Sessions/Sessions.tsx
@@ -43,6 +43,56 @@ function Sessions() {
           </div>
         </div>
       </section>
+      <section className="detailsSection">
+        <p>
+          <span>Includes:</span> <br />
+          <br />
+          High-resolution images <br />
+          <br />
+          All photos edited and delivered digitally <br />
+          <br />
+          Gallery delivered within 1-2 weeks (4-6 for weddings) <br />
+          <br />
+          Outfit change if wanted/needed <br />
+          <br />
+          Indoor and/or outdoor setting <br />
+          <br />
+          Fun and loving atmosphere <br />
+          <br />
+          Personalized questionaire
+        </p>
+        <p>
+          <span>Add-Ons:</span> <br />
+          <br />
+          <span className="bold">
+            Location that requires travel and/or hotel
+          </span>
+          <br />
+          <br />
+          I love to capture photos in new places <br />
+          <br />
+          <span className="bold">
+            Additional time <br />
+          </span>
+          <br />
+          I want to be able to make your session everything you want/need <br />
+          <br />
+          <span className="bold">
+            Special request <br />
+          </span>
+          <br />
+          Whether its champagne, flowers, a picnic, a sign, etc. I am your girl
+        </p>
+      </section>
+      <section className="backgroundImgSection">
+        <span className="bottomTextSm">MEMORIES WORTH EVERYTHING</span>
+        <p className="bottomTextLg">
+          I WOULD LOVE TO CAPTURE ALL OF YOUR LOVELY MOMENTS
+        </p>
+        <a className="bottomTextLink" href="/contact">
+          CONTACT ME
+        </a>
+      </section>
     </main>
   );
 }
