4cf717e84890b51dc58b0ad8ad26177fbc77f153
Jonathon Higham <109483370+jsshigham@users.noreply.github.com>
2024-01-16 15:44:37 -0700

    contact page finished

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,5 +1,64 @@
+import "./contact.css";
+
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <main>
+      <section className="section1">
+        <img
+          src="public/aboutPage/sieraWithCamera.jpeg"
+          alt="Siera with camera"
+        />
+        <div className="section1Right">
+          <span className="section1RightHeading">
+            DON'T HESITATE, <br /> CONTACT ME FOR ANY <br /> INFORMATION
+          </span>
+          <p>
+            <span>
+              INQUIRIES: <br />
+              <br />
+            </span>
+            <a
+              href="https://unscriptedforphotographers.com/b/OwxrE2lhk9nVqJ"
+              className="section1Btn"
+            >
+              COMPLETE THIS BOOKING FORM
+            </a>
+          </p>
+          <p>
+            <span>
+              FOLLOW: <br />
+              <br />
+            </span>
+            <a
+              href={`https://www.instagram.com/si.lovelymoments?igsh=NWxhbnUyOGhianRr&utm_source=qr`}
+              className="section1Btn"
+            >
+              INSTAGRAM @si.lovelymoments
+            </a>
+            <i></i>
+          </p>
+          <p>
+            <span>
+              {" "}
+              PURCHASE: <br />
+              <br />
+            </span>
+            <a
+              href={`https://silovelymoments.etsy.com/`}
+              className="section1Btn"
+            >
+              LANDSCAPES AVAILIBLE THROUGH ETSY HERE
+            </a>
+          </p>
+        </div>
+      </section>
+      <img
+        className="momentsLogo"
+        src="/si.lovelymoments_logo.png"
+        alt="Si Lovely Moments Logo"
+      />
+    </main>
+  );
 }
 
 export default Contact;
