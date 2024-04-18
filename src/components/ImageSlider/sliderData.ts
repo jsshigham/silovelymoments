e7821a60d538f9fbfc9d68e437afcc584a19fc41
Jonathon Higham <109483370+jsshigham@users.noreply.github.com>
2024-04-17 19:50:38 -0500

    image slider 2 phantom images

--- src/components/ImageSlider/sliderData.ts
+++ src/components/ImageSlider/sliderData.ts
@@ -1,5 +1,5 @@
 export const imageSliderImgs: string[] = [];
 
-for (let index = 0; index < 94; index++) {
+for (let index = 0; index < 92; index++) {
   imageSliderImgs.push(`imgg${index}.jpg`);
 }
